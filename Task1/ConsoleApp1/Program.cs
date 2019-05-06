d5e02e0d46a5a2dfa6a830918ab33a5d065e090d
taras zvaruch <yurko009@ukr.net>
2019-05-06 20:59:18 +0300

    initial commit

---
+++ Task1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < width - i; j++)
+                {
+                    Console.Write(number.ToString());
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
